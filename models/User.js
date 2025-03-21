2ff549c3e7caa02f26638977cbaf3fa809ec9347
dkikowe <dkikowe@gmail.com>
2025-03-22 04:13:04 +0500

    s

--- models/User.js
+++ models/User.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       enum: ["Грузодатель", "Грузоперевозчик", "Диспетчер"],
+      default: "",
     },
     telegramId: { type: String, unique: true, required: true },
     avatar: { type: String, default: "" },
